e59fbd2e97ccd9c4ffa7b78d4f3de67f002e1f33
kjminn <kjminn2001@gmail.com>
2025-05-28 11:41:39 +0900

    10장 Security(도서등록)

---
+++ src/main/java/kr/ac/kopo/minn/bookmarket/validator/BookIdValidator.java
@@ -0,0 +1,34 @@
+package kr.ac.kopo.minn.bookmarket.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import kr.ac.kopo.minn.bookmarket.domain.Book;
+import kr.ac.kopo.minn.bookmarket.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class BookIdValidator  implements ConstraintValidator<BookId, String> {
+    @Autowired
+    private BookService bookService;
+
+    @Override
+    public void initialize(BookId constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        Book book = null;
+        try {
+            book = bookService.getBookById(value);
+        }catch (RuntimeException e){
+            return true;
+        }
+
+        if(book != null){
+            return false;
+        }
+
+        return true;
+    }
+}
